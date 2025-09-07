6ca9881a44aa3d37d3fd657dfa2fc5b4af2290b0
md mohsin <siam.mohsin2005@gmail.com>
2025-09-07 10:24:41 +0600

    Basic Flask Application

---
+++ Flask/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# URL Binding
+
+@app.route('/')
+def hello():
+    return("Hello, I am Mohsin")
+
+@app.route('/page2')
+def page():
+    return("This is page 2")
+
+@app.route('/admin')
+def hello_2():
+    return("I am Admin!!")
+
+app.run(debug=True)
